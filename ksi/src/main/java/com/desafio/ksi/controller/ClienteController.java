232167c70a0e459e13691b51071ea75e4a2a4be9
Alexandro <alexlmangabeira@gmail.com>
2023-04-06 00:07:07 -0300

    Criando Controller

---
+++ ksi/src/main/java/com/desafio/ksi/controller/ClienteController.java
@@ -0,0 +1,12 @@
+package com.desafio.ksi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/cliente")
+@Controller
+@CrossOrigin("*")
+public class ClienteController {
+
+}
