8bee9fdae25f6d56a4e9adf88a213ead5988bb66
Alexandro <alexlmangabeira@gmail.com>
2023-04-05 23:53:18 -0300

    Criando Entity

---
+++ ksi/src/main/java/com/desafio/ksi/entity/Cliente.java
@@ -0,0 +1,27 @@
+package com.desafio.ksi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@Entity
+@NoArgsConstructor
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email")
+	private String email;
+}
